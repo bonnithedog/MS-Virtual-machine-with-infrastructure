c6473c5f1a3311ea413c9281a5f3c22ab37da398
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-07-27 08:38:16 +0200

    Update terraform_azure.tf

--- terraform_azure.tf
+++ terraform_azure.tf
@@ -1,6 +1,7 @@
 #File terraform_azure.tf
 #https://docs.microsoft.com/en-us/azure/developer/terraform/create-linux-virtual-machine-with-infrastructure
 # Configure the Microsoft Azure Provider
+
 provider "azurerm" {
     # The "feature" block is required for AzureRM provider 2.x. 
     # If you're using version 1.x, the "features" block is not allowed.
